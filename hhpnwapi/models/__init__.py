b96cd2cd79d1d7e934d752bb22f6650a26c50068
Stacey Vanyo <sbvanyo@gmail.com>
2024-01-08 11:21:39 -0600

    models created and migrated

---
+++ hhpnwapi/models/__init__.py
@@ -0,0 +1,5 @@
+from .item import Item
+from .order_item import OrderItem
+from .order import Order
+from .revenue import Revenue
+from .user import User
---
+++ hhpnwapi/models/user.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+class User(models.Model):
+
+    name = models.CharField(max_length=50)
+    uid = models.CharField(max_length=50)
